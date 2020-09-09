69d353bde1fc7ff31eb739a2d8d9ecfd0b8abb94
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-09-09 15:28:35 -0300

    initial commit - base project

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+import express, { Request, Response } from "express";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+// PARA TESTAR A CONEXÃO COM O BANCO DE DADOS
+app.get("/teste", async (req: Request, res: Response) => {
+  try {
+    res.status(200).send("Oi, seu server está funcionando!");
+  } catch (error) {
+    res.status(400).send("ERRO");
+  }
+});
+
+// PARA LINKAR O BANCO DE DADOS COM O CÓDIGO
+const server = app.listen(3000, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando em http://localhost:${address.port}`);
+  } else {
+    console.error(`Falha ao rodar o servidor.`);
+  }
+});
